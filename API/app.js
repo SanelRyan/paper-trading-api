1c870438c4a5d6cda7619d60796e2da7a0596237
Sanel Ryan <sanelryantclitus@gmail.com>
2024-08-26 23:23:28 +0530

    Cors Update

--- API/app.js
+++ API/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
@@ -7,6 +8,7 @@ const { v4: uuidv4 } = require("uuid");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/createAccount", (req, res) => {
